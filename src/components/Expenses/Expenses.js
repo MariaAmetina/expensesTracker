bca6afd7b1be4196e245a389f65d19b6e9421320
MariiaAmetina <ametina.ms@gmail.com>
2021-11-13 12:39:40 +0300

    первая версия приложения по учету трат

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import Card from "./../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+import "./Expenses.css";
+
+const Expenses = ({ items }) => {
+  const [filteredYear, setfilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setfilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
